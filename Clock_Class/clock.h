60091245f3cb1a993c1a8024fefa25c6175f9d0b
dmccain04 <dmccain04@gmail.com>
2016-12-02 13:37:37 -0500

    Initial Setup of Header File
    Add class definition & header comments

---
+++ Clock_Class/clock.h
@@ -0,0 +1,34 @@
+// FILE: clock.h
+// CLASS PROVIDED: clock (part of the namespace main_mccain_2A)
+//
+// CONSTRUCTORS for the clock class:
+//
+// MODIFICATION MEMBER FUNCTIONS for the clock class:
+//
+// CONSTANT MEMBER FUNCTIONS for the clock class:
+//
+// VALUE SEMANTICS for the clock class:
+// 		Assignments and the coy constructor may be used with clock objects. 
+
+#ifndef MAIN_MCCAIN_CLOCK
+#define MAIN_MCCAIN_CLOCK
+
+namespace main_mccain_2A
+{
+	class clock {
+	public:
+		// CONSTRUCTORS
+		
+		// MODIFICATION MEMBER FUNCTIONS
+		
+		// CONSTANT MEMBER FUNCTIONS
+	
+	
+	private:
+	
+	};
+
+}
+
+
+#endif
